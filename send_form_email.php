206f3bb636b09e153013e9cfd60140cdf7e1a008
BrandLadder <43156478+BrandLadder@users.noreply.github.com>
2018-09-13 16:09:13 -0400

    form

---
+++ send_form_email.php
@@ -0,0 +1,24 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: BrandLadder.com'; 
+    $to = 'alex@brandladderdm.com'; 
+    $subject = 'Urequa';
+			
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+				
+	if ($_POST['submit']) {
+		if ($name != '' && $email != '') {	 
+				if (mail ($to, $subject, $body, $from)) { 
+				echo '<p>Your message has been sent!</p>';
+				}
+			else { 
+				echo '<p>Something went wrong, go back and try again!</p>'; 
+			} 
+		} 
+		 else {
+			echo '<p>You need to fill in all required fields!</p>';
+		}
+	}
+			?>
